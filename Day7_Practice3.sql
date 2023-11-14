0e0fada3d68caf910378979f0e7b1b71e91ef3b0
PhatPham2K <131521072+PhatPham2K@users.noreply.github.com>
2023-11-14 23:52:10 +0700

    Update Day7_Practice3.sql

--- Day7_Practice3.sql
+++ Day7_Practice3.sql
@@ -1,1 +1,56 @@
+--ex1
+select name from students
+where marks > 75
+order by right(name,3), id 
 
+--ex2
+SELECT Users.user_id , CONCAT(UPPER(SUBSTR(Users.name,1,1)),LOWER(SUBSTR(Users.name,2))) AS name 
+FROM Users
+ORDER BY
+Users.user_id ASC
+
+  
+--ex3
+SELECT manufacturer,
+('$' || round(sum(total_sales)/ 1000000) || ' million') as sale
+FROM pharmacy_sales
+group by 1 
+ORDER BY SUM(total_sales) desc;
+
+--ex4
+SELECT EXTRACT(MONTH FROM submit_date) AS month,
+product_id AS product,
+ROUND(AVG(stars), 2) AS avg_value
+FROM reviews
+GROUP BY EXTRACT(MONTH FROM submit_date),product_id
+ORDER BY month,product;
+
+--ex5
+SELECT sender_id,count(message_id) FROM messages
+where extract(month from sent_date) = '8' and extract(year from sent_date)= '2022'
+group by 1 order by 2 desc limit 2;
+
+--ex6
+select tweet_id  from Tweets where length(content) >15;
+
+--ex7
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
+GROUP BY activity_date;
+
+--ex8
+SELECT COUNT(id) AS number_employee
+FROM employees
+WHERE EXTRACT(MONTH FROM joining_date) BETWEEN 1 AND 7
+AND EXTRACT(YEAR FROM joining_date) = 2022;
+
+--ex9
+SELECT POSITION('a' IN LOWER(first_name)) AS position_of_a
+FROM employees
+WHERE first_name = 'Amitah';
+
+--ex10
+select substring(title, length(winery)+2,4) 
+from winemag_p2
+where country 'Macedonia'
