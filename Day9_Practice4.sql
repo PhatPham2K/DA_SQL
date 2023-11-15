ab45420d5f64a809e1839ab4f9b3d0373fe87f8b
PhatPham2K <131521072+PhatPham2K@users.noreply.github.com>
2023-11-16 01:38:12 +0700

    Update Day9_Practice4.sql

--- Day9_Practice4.sql
+++ Day9_Practice4.sql
@@ -24,6 +24,67 @@ count(*)
 from bookings.flights
 group by season
 
-
-
 *** Practice
+--ex1: datalemur-laptop-mobile-viewership.
+SELECT 
+  SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END)  laptop_views, 
+  SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END)  mobile_views 
+FROM viewership;
+
+--ex2: datalemur-triangle-judgement.
+SELECT x,y,z,
+    case when (x+y)>z and x+z>y and z+y>x   then 'Yes' else 'No' 
+    end as 'triangle'
+from Triangle
+
+
+--ex3: datalemur-uncategorized-calls-percentage.
+SELECT
+    ROUND(SUM(CASE 
+      WHEN call_category IS NULL THEN 1 
+      WHEN call_category = 'n/a' THEN 1
+      ELSE 0
+    END) * 100.0 / COUNT(case_id), 1)
+FROM callers
+
+--ex4: datalemur-find-customer-referee.
+SELECT name
+FROM customer
+WHERE 
+    CASE 
+        WHEN referee_id = 2 THEN 0
+        ELSE 1
+    END = 1 OR referee_id IS NULL;
+
+--ex5: stratascratch the-number-of-survivors.
+SELECT
+    survived,
+    COUNT(CASE WHEN pclass = 1 THEN 1 END) AS first_class,
+    COUNT(CASE WHEN pclass = 2 THEN 1 END) AS second_class,
+    COUNT(CASE WHEN pclass = 3 THEN 1 END) AS third_class
+FROM titanic
+GROUP BY survived;
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
